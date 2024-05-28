aeb60ad9939224b4594e6926d35f80bac1e1a569
Juerg Hauser <juerg.hauser@gmail.com>
2024-05-28 11:18:49 +1000

    chore: Apply GPL3

--- src/pyp223/_pyp223.py
+++ src/pyp223/_pyp223.py
@@ -1,22 +1,21 @@
- Copyright (C) 2017 CSRIO
-!
-! This file is part of PyP223
-!
-! PyP223 is free software: you can redistribute it and/or modify
-! it under the terms of the GNU General Public License Version 2
-! as published by the Free Software Foundation.
-!
-! PyP223 is distributed in the hope that it will be useful,
-! but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-! GNU General Public License for more details.
-!
-! You should have received a copy of the GNU General Public License
-! along with PyP223. If not, see <http://www.gnu.org/licenses/>.
-!
-! To contact CSIRO about this software you can e-mail
-! juerg.hauser@csiro.au
-
+# Copyright (C) 2017-2024 CSRIO
+#
+# This file is part of PyP223
+#
+# PyP223 is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# PyP223 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with PyP223.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+# To contact CSIRO about this software you can e-mail
+# juerg.hauser@csiro.au
+#
 
 
 import os
